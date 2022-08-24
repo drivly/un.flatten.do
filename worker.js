d3b05580e48be50ae8a9f3852a16df46e901ab16
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 13:56:04 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,6 +3,7 @@ import flatten from 'flat'
 export default {
   fetch: async req => {
     const { pathname } = new URL(req.url)
+    console.log('https://' + pathname)
     const data = await fetch('https://' + pathname).then(res => res.json())
     return new Response(JSON.stringify(flatten(data), null, 2), { headers: { 'content-type': 'application/json' }})
   }
